fb0ce90d356f705ade5a25fb4bdc529426e939e0
dasushan <53913896+dasushan@users.noreply.github.com>
2024-03-05 11:00:47 +0530

    new file added

---
+++ src/strings/PrefixesofString.java
@@ -0,0 +1,26 @@
+package strings;
+
+import java.util.HashSet;
+
+public class PrefixesofString {
+    public static void main(String[] args) {
+        String s = "abc";
+        String[] words = {"a","b","c","ab","bc","abc"};
+        System.out.println(countPrefixes(words, s));
+
+    }
+
+    public static int countPrefixes(String[] words, String s){
+        HashSet<String> hset = new HashSet<>();
+        for(int i=s.length(); i>=1; i--){
+            hset.add(s.substring(0, i));
+        }
+        int cnt =0;
+        for(int i=0; i<words.length; i++){
+            if(hset.contains(words[i])){
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
